31096b24ee663012d4855905831b9ec07becd6d0
Vae22 <1098292108@qq.com>
2022-01-27 15:27:04 +0800

    单元测试依赖

--- src/main/java/test/UserControllerTest.java
+++ src/main/java/test/UserControllerTest.java
@@ -5,6 +5,7 @@ import com.example.demo.DemoApplication;
 import com.example.demo.entity.User;
 import com.example.demo.mapper.UserMapper;
 import org.junit.Test;
+import org.testng.annotations.AfterTest;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
